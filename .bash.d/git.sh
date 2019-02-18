7270f900721de8da6115e041cd1c2eda4afdddd9
Novel Corpse <romandafe94@gmail.com>
2019-02-18 22:29:02 +0300

    refs #1; reworked git-acp function;

--- .bash.d/git.sh
+++ .bash.d/git.sh
@@ -5,35 +5,20 @@ function parse_git_branch() {
 }
 
 function git-acp() {
-  local base=`echo $(parse_git_branch) | cut -d'/' -f 2 | cut -c 2- | rev | cut -c 2- | rev` 
-  if [ -z $(parse_git_branch) ]; then
+  local b=`git rev-parse --abbrev-ref HEAD`
+  if [ -z $b ]; then
     printf "\033[1;32m[i]: branch not found. move to local repository.\033[0m\n"
     return
-  else if [ "$base" = "master" ]; then
-    printf "\033[01;38;05;214m[w]: it is master branch; auto checkout on default feature branch...\033[0m\n"
-    git checkout -b "feature/master/#0"
-    printf "\033[1;32m[i]: default feature branch has been created.\033[0m\n"
-    fi
   fi
-  
-  local branch=`echo $(parse_git_branch) | cut -c 2- | rev | cut -c 2- | rev`
 
-  local kind=`echo $(parse_git_branch) | cut -d'/' -f 1 | cut -c 2- `  
-  if [ $kind = "release" ]; then
-    printf "\033[01;38;05;214m[w]: it is release branch; please checkout on feature branch.\033[0m\n"
-    version=`echo $(parse_git_branch) | cut -d'/' -f 2 | rev | cut -c 2- | rev`
-    echo $version
+  local kind=`echo $b | cut -d'/' -f 1 | cut -c 1- `
+  if [ $kind == "feature" ]; then
+    printf "\033[1;32m[i]: it is feature branch: add, commit, push!\033[0m\n"
+    local msg="refs `echo $b | cut -d'/' -f 3`; $1;"
+    git add . && git commit -m "$msg" && git push origin $branch
     return
   fi
 
-  local number=`echo $(parse_git_branch) | cut -d'/' -f 3 | rev | cut -c 2- | rev`
- 
-  if [ -z $number ]; then
-    printf "\033[01;38;05;214m[w]: number of ticket not found;\033[0m\n"
-    number=`echo '#0'`
-  fi 
-  
-  local msg="refs $number; $1;"
-  git add . && git commit -m "$msg" && git push origin $branch
-  printf "\033[1;32m[i]: changes sent to remote successfully!\033[m\n"
+  printf "\033[1;32m[w]: something wrong; check correct branch name;\033[0m\n"
+  return
 }
