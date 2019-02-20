0baa8bbdcf38dc810b4cbb677612a7a60eacb84e
Novel Corpse <romandafe94@gmail.com>
2019-02-20 23:44:31 +0300

    refs #1; tiny fixes cpu.sh; removed is_cygwin condition;

--- .tmux/cpu.sh
+++ .tmux/cpu.sh
@@ -1,18 +1,14 @@
 #!/usr/bin/env bash
 
-is_osx() {
+function is_osx() {
 	[ $(uname) == "Darwin" ]
 }
 
-is_linux() {
+function is_linux() {
 	[ $(uname) == "Linux" ]
 }
 
-is_cygwin() {
-	command -v WMIC &> /dev/null
-}
-
-cpus_number() {
+function cpus_number() {
 	if is_linux; then
 		nproc
 	else
@@ -20,18 +16,11 @@ cpus_number() {
 	fi
 }
 
-cpu_load() {
-	if is_osx; then
+function cpu_load() {
+	if [ is_osx ] || [ is_linux ]; then
 		load=`ps aux | awk '{print $3}' | tail -n+2 | awk '{s+=$1} END {print s}'`
 		cpus=$(cpus_number)
 		echo "$load $cpus" | awk '{printf "%5.2f%%\n", $1/$2}'
-	elif is_cygwin; then
-		usage="$(WMIC cpu get LoadPercentage | grep -Eo '^[0-9]+')"
-		printf "%5.1f%%" $usage
-	else
-		load=`ps -aux | awk '{print $3}' | tail -n+2 | awk '{s+=$1} END {print s}'`
-		cpus=$(cpus_number)
-		echo "$load $cpus" | awk '{printf "%5.2f%%", $1/$2}'
 	fi	
 }
 cpu_load
