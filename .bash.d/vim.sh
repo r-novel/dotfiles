67947b7e8c28b247effe36eedad0875f9cdd59d2
Roman Romanenko <romandafe94@gmail.com>
2020-05-07 22:25:35 +0300

    refs #0; vim hints started;

--- .bash.d/vim.sh
+++ .bash.d/vim.sh
@@ -7,3 +7,8 @@ alias to-dotvim='cd $HOME/.vim'
 
 # open vim Go IDE;
 alias vimgo='vim -u ~/.vimrc.go'
+
+function vim-help() {
+	printf "\033[4;49;32m USAGE:\033[0m\n"
+	printf "\033[38;5;39m\033[0m\033[5;49;36m:line_num1,line_num2t.<enter>\033[0m \t\t/* copy the current lines to your present cursor location; */\n"
+}
